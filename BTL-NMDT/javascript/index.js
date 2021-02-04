710c70c4c5b728011bc69a115db8c142afb9c778
Ha Vinh Nguyen <54196381+meterglost@users.noreply.github.com>
2021-02-05 00:21:23 +0700

    Add files via upload

--- BTL-NMDT/javascript/index.js
+++ BTL-NMDT/javascript/index.js
@@ -1,5 +1,17 @@
 window.onload = () => {
     document.querySelector(".main").style.marginLeft = "30%"
-    document.querySelector(".sidebar").style.display = "flex"
-    
+    document.querySelector(".sidebar").style.display = "flex"   
+}
+
+document.querySelector(".sidebar-button").onclick = () => {
+    if (document.querySelector(".sidebar-button").textContent.charCodeAt(0) == 9664) {
+        document.querySelector(".tableofcontents").style.display = "none"
+        document.querySelector(".main").style.removeProperty("margin-left")
+        document.querySelector(".sidebar-button").textContent = String.fromCharCode(9654)
+    }
+    else {
+        document.querySelector(".main").style.marginLeft = "30%"
+        document.querySelector(".tableofcontents").style.display = "block"
+        document.querySelector(".sidebar-button").textContent = String.fromCharCode(9664)
+    }
 }
